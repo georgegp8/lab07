c6a55658c486984e0552d2b423240cb4007adb45
george <george.guerra@tecsup.edu.pe>
2025-04-28 11:57:28 -0500

    feat: implementar pantalla ScreenUser con inserción y listado de usuarios usando Room

--- app/src/main/java/com/example/datossinmvvm/MainActivity.kt
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -1,17 +1,35 @@
 package com.example.datossinmvvm
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.room.Room
 import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +38,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DatosSinMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ScreenUser(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +48,102 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ScreenUser(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var db: UserDatabase
+    var id        by remember { mutableStateOf("") }
+    var firstName by remember { mutableStateOf("") }
+    var lastName  by remember { mutableStateOf("") }
+    var dataUser  = remember { mutableStateOf("") }
+
+    db = crearDatabase(context)
+
+    val dao = db.userDao()
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        Spacer(Modifier.height(50.dp))
+        TextField(
+            value = id,
+            onValueChange = { id = it },
+            label = { Text("ID (solo lectura)") },
+            readOnly = true,
+            singleLine = true
+        )
+        TextField(
+            value = firstName,
+            onValueChange = { firstName = it },
+            label = { Text("First Name: ") },
+            singleLine = true
+        )
+        TextField(
+            value = lastName,
+            onValueChange = { lastName = it },
+            label = { Text("Last Name:") },
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                val user = User(0,firstName, lastName)
+                coroutineScope.launch {
+                    AgregarUsuario(user = user, dao = dao)
+                }
+                firstName = ""
+                lastName = ""
+            }
+        ) {
+            Text("Agregar Usuario", fontSize=16.sp)
+        }
+        Button(
+            onClick = {
+                val user = User(0,firstName, lastName)
+                coroutineScope.launch {
+                    val data = getUsers( dao = dao)
+                    dataUser.value = data
+                }
+            }
+        ) {
+            Text("Listar Usuarios", fontSize=16.sp)
+        }
+        Text(
+            text = dataUser.value, fontSize = 20.sp
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    DatosSinMVVMTheme {
-        Greeting("Android")
+fun crearDatabase(context: Context): UserDatabase {
+    return Room.databaseBuilder(
+        context,
+        UserDatabase::class.java,
+        "user_db"
+    ).build()
+}
+
+suspend fun getUsers(dao:UserDao): String {
+    var rpta: String = ""
+    //LaunchedEffect(Unit) {
+    val users = dao.getAll()
+    users.forEach { user ->
+        val fila = user.firstName + " - " + user.lastName + "\n"
+        rpta += fila
     }
+    //}
+    return rpta
+}
+
+suspend fun AgregarUsuario(user: User, dao:UserDao): Unit {
+    //LaunchedEffect(Unit) {
+    try {
+        dao.insert(user)
+    }
+    catch (e: Exception) {
+        Log.e("User","Error: insert: ${e.message}")
+    }
+    //}
 }
